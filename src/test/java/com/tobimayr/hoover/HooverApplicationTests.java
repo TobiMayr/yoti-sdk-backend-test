813e1c337e67e06ac25bbf778c0e8392f5118dc1
TobiMayr <tobistian@gmail.com>
2022-02-13 00:38:06 +0100

    initial commit

---
+++ src/test/java/com/tobimayr/hoover/HooverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobimayr.hoover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HooverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
